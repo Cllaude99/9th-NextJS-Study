08e0007dc14ba080a58891cbd12a00af40e17022
TaeYoon Kim <xodbsrla1025@naver.com>
2024-03-26 19:15:44 +0900

    feat: "ThemeProvider + GlobalStyle 설정"

--- Cllaude99/nextjs-app-1/app/page.tsx
+++ Cllaude99/nextjs-app-1/app/page.tsx
@@ -1,3 +1,11 @@
+'use client';
+
+import styled from 'styled-components';
+
 export default function Home() {
-  return <div>hello</div>;
+  return <Container>hello</Container>;
 }
+
+const Container = styled.div`
+  color: ${(props) => props.theme.black.darker};
+`;
---
+++ Cllaude99/nextjs-app-1/styles/theme.ts
@@ -0,0 +1,14 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  red: 'tomato',
+  black: {
+    veryDark: '#141414',
+    darker: '#181818',
+    lighter: '#2F2F2F',
+  },
+  white: {
+    lighter: '#fff',
+    darker: '#989898',
+  },
+};
